90ca6422a5d5b457d713c8fc7df8c4e883288006
shatilov-makar <62944516+shatilov-makar@users.noreply.github.com>
2020-04-05 15:16:04 +0500

    Update main.cpp

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -6,22 +6,20 @@ using namespace std;
 //Shatilov M.R. 
 
 
-struct statistic
+struct statistic 
 {
-    statistic()
-    {
-        winCrosses;
-        winNoughts;
-        draws;
-    }
     int winCrosses = 0;
     int winNoughts = 0;
     int draws = 0;
 };
 
+const int maxChildsAfterFirstStep = 8;
+const int sideLength = 3;
+const int countCells = 9;
+
 void makeStep(TreeNode&, statistic*);
 
-statistic* getStatistic(TreeNode node, statistic* stat) 
+statistic* getStatistic(TreeNode node, statistic* stat)
 {
     if (node.isTerminal())
     {
@@ -40,7 +38,7 @@ statistic* getStatistic(TreeNode node, statistic* stat)
         return stat;
     }
     makeStep(node, stat);
-    if (node.childCount() == 8 )
+    if (node.childCount() == maxChildsAfterFirstStep)
     {
         cout << "You can win in " << stat->winCrosses << " ways,  you can lose in " << stat->winNoughts << " ways, and will be a draw " << stat->draws << " ways" << endl << endl;
         stat->draws = 0; stat->winCrosses = 0; stat->winNoughts = 0;
@@ -51,9 +49,9 @@ statistic* getStatistic(TreeNode node, statistic* stat)
 void makeStep(TreeNode& node, statistic* stat)
 {
     int row = 0; int column = 0;
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < countCells; i++)
     {
-        row = i / 3; column = i % 3;
+        row = i / sideLength; column = i % sideLength;
         auto condition = node.value()->operator()(row, column);
         if (condition == PlayField::cellCondition::csEmpty)
         {
