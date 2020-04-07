e07e966e8b71780ed7d0f13439fe3cc5fe08a09a
shatilov-makar <62944516+shatilov-makar@users.noreply.github.com>
2020-04-07 12:12:38 +0500

    Update main.cpp

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -6,47 +6,44 @@ using namespace std;
 //Shatilov M.R. 
 
 
-struct statistic 
+struct result
 {
-    int winCrosses = 0;
-    int winNoughts = 0;
-    int draws = 0;
+int wins = 0;
+int loses = 0;
+int draws = 0;
+result& operator +=(const result& ir)
+{
+    wins += ir.wins;
+    loses += ir.loses;
+    draws += ir.draws;
+    return *this;
+}
 };
 
 const int maxChildsAfterFirstStep = 8;
 const int sideLength = 3;
 const int countCells = 9;
 
-void makeStep(TreeNode&, statistic*);
+void makeStep(TreeNode, result*);
 
-statistic* getStatistic(TreeNode node, statistic* stat)
+result getStatistic(TreeNode node)
 {
+    result res;
     if (node.isTerminal())
     {
         if (node.value()->checkFieldStatus() == PlayField::mapCondition::fsCrossesWin)
-        {
-            stat->winCrosses++;
-            return stat;
-        }
-        if (node.value()->checkFieldStatus() == PlayField::mapCondition::fsNoughtsWin)
-        {
-            stat->winNoughts++;
-            return stat;
-        }
+            res.wins++;
+        else if (node.value()->checkFieldStatus() == PlayField::mapCondition::fsNoughtsWin)
+            res.loses++;
         else
-            stat->draws++;
-        return stat;
+            res.draws++;
+        return res;
     }
-    makeStep(node, stat);
-    if (node.childCount() == maxChildsAfterFirstStep)
-    {
-        cout << "You can win in " << stat->winCrosses << " ways,  you can lose in " << stat->winNoughts << " ways, and will be a draw " << stat->draws << " ways" << endl << endl;
-        stat->draws = 0; stat->winCrosses = 0; stat->winNoughts = 0;
-        return stat;
-    }
+    makeStep(node, &res);
+    return res;
 }
 
-void makeStep(TreeNode& node, statistic* stat)
+void makeStep(TreeNode node, result* res)
 {
     int row = 0; int column = 0;
     for (int i = 0; i < countCells; i++)
@@ -56,15 +53,20 @@ void makeStep(TreeNode& node, statistic* stat)
         if (condition == PlayField::cellCondition::csEmpty)
         {
             auto childNode = node.addChild(PlayField::CellIdx(row, column));
-            getStatistic(childNode, stat);
+            *res += getStatistic(childNode);
         }
+    }
+    if (node.childCount() == maxChildsAfterFirstStep)
+    {
+        cout << "You can win in " << res->wins << " ways,  you can lose in " << res->loses << " ways, and will be a draw " << res->draws << " ways" << endl << endl;
+        res->draws = 0; res->wins = 0; res->loses = 0;
     }
 }
 
 int main()
 {
     TreeNode rootNode(nullptr, PlayField());
-    statistic statistic;
-    getStatistic(rootNode, &statistic);
+    result res;
+    makeStep(rootNode, &res);
     return 0;
 }
